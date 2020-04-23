69c6495ba496a2dc45e72349aa7680f7d2f421e1
Ramzy Rashaun Arief <ramzy@Ramzys-MacBook-Pro.local>
2020-04-24 00:02:34 +0700

    Initial project setup

---
+++ ShareRide/Controller/LoginController.swift
@@ -0,0 +1,107 @@
+//
+//  LoginController.swift
+//  ShareRide
+//
+//  Created by Ramzy Rashaun Arief on 22/04/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SHARERIDE"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.anchor(height: 50)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.anchor(height: 50)
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceHolder: "Email",
+                                       isSecureTextEntry: false)
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceHolder: "Password",
+                                       isSecureTextEntry: true)
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
+        button.backgroundColor = .mainBlueTint
+        button.layer.cornerRadius = 5
+        button.anchor(height: 50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint]))
+        
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .backgroundColor
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 16)
+        titleLabel.centerX(inView: view)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 24
+        
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 40, paddingLeft: 16,
+                     paddingRight: 16)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
+        
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp() {
+        
+    }
+    
+}
---
+++ ShareRide/Controller/SignUpController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpController.swift
+//  ShareRide
+//
+//  Created by Ramzy Rashaun Arief on 23/04/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    // MARK: - Selectors
+    
+}
