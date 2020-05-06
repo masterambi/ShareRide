dea5bc20c81fedb42e2e49422712cfbb795ca492
masterambi <ramzyrash@gmail.com>
2020-05-06 19:47:46 +0700

    Fix zoom to fit annotation functionality when the RideActionView is displayed

--- ShareRide/Controller/HomeController.swift
+++ ShareRide/Controller/HomeController.swift
@@ -431,7 +431,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             self.mapView.selectAnnotation(annotation, animated:true)
             
             let annotations = self.mapView.annotations.filter { !$0.isKind(of: DriverAnnotation.self) }
-            self.mapView.showAnnotations(annotations, animated: true)
+            self.mapView.zoomToFit(annotations: annotations)
             
             self.animateRideActionView(shouldShow: true, destination: selectedPlacemark)
         }
