9cf115a17e05aaa4f294a1304d67beaf9d0c8bc8
masterambi <ramzyrash@gmail.com>
2020-05-08 14:10:22 +0700

    Add pickup annotation to mapView

--- ShareRide/Controller/PickupController.swift
+++ ShareRide/Controller/PickupController.swift
@@ -58,6 +58,7 @@ class PickupController: UIViewController {
         super.viewDidLoad()
                 
         configureUI()
+        configureMapView()
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -77,6 +78,16 @@ class PickupController: UIViewController {
     // MARK: - API
     
     // MARK: - Helper Functions
+    
+    func configureMapView() {
+        let region = MKCoordinateRegion(center: trip.pickupCoordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: false)
+        
+        let anno = MKPointAnnotation()
+        anno.coordinate = trip.pickupCoordinates
+        mapView.addAnnotation(anno)
+        mapView.selectAnnotation(anno, animated: true)
+    }
     
     func configureUI() {
         view.backgroundColor = .backgroundColor
