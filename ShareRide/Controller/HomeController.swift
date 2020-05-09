677442ede0ce1981187fe7efbcaf7503a3f84c6c
masterambi <ramzyrash@gmail.com>
2020-05-09 15:23:12 +0700

    Add loading view during trip request on user side

--- ShareRide/Controller/HomeController.swift
+++ ShareRide/Controller/HomeController.swift
@@ -473,13 +473,18 @@ extension HomeController: RideActionViewDelegate {
     func uploadTrip(_ view: RideActionView) {
         guard let pickupCoordinates = locationManager?.location?.coordinate else { return }
         guard let destinationCoordinates = view.destination?.coordinate else { return }
+        
+        shouldPresentLoadingView(true, message: "Finding you a ride..")
+        
         Service.shared.uploadTrip(pickupCoordinates, destinationCoordinates) { (err, ref) in
             if let error = err {
                 print("DEBUG: Failed to upload trip with error \(error)")
                 return
             }
             
-            print("DEBUG: Did upload trip successfully")
+            UIView.animate(withDuration: 0.3, animations: {
+                self.rideActionView.frame.origin.y = self.view.frame.height
+            })
         }
     }
 }
