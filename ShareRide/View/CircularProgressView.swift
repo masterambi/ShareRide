14ae1967c760af0d0b800fd9fe4e1cdcd9c06e20
masterambi <ramzyrash@gmail.com>
2020-05-25 13:40:44 +0700

    Set up circular progress view

---
+++ ShareRide/View/CircularProgressView.swift
@@ -0,0 +1,9 @@
+//
+//  CircularProgressView.swift
+//  ShareRide
+//
+//  Created by Ramzy Rashaun Arief on 25/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
